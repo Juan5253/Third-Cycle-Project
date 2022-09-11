7827067e909db6098cb122a34a50ff14b8ffe2e0
Juan5253 <juan_sebastianf@hotmail.com>
2022-09-11 16:58:38 -0500

    DB Validation using migration

--- App/Controller/Migrations/20220911215140_Validations.Designer.cs
+++ App/Controller/Migrations/20220911215140_Validations.Designer.cs
@@ -3,14 +3,16 @@ using Controller.AppRepositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Controller.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20220911215140_Validations")]
+    partial class Validations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- App/Model/entities/Municipio.cs
+++ App/Model/entities/Municipio.cs
@@ -2,14 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Model.entities
 {
     public class Municipio
     {   
         //Use of Entity Framework
+        //[key] Primary Key notation
         public int id { get; set; }
+        [Required(ErrorMessage="El campo Nombre es obligatorio")]
+        [MaxLength(30,ErrorMessage="El campo {0} no puede tener más de {1} caracteres")]
+        [MinLength(4,ErrorMessage="El campo {0} no puede tener menos de {1} caracteres")]
         public string nombre { get; set; }
+        [MaxLength(40,ErrorMessage="El campo {0} no puede tener más de {1} caracteres")]
+        [MinLength(10,ErrorMessage="El campo {0} no puede tener menos de {1} caracteres")]
         public string secretaria { get; set; }
         /*
         //OOP Native
