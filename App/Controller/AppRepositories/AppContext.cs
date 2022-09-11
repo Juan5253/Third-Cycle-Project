d20ee5b394672dcd47e47ef74be3913ed603bacb
Juan5253 <juan_sebastianf@hotmail.com>
2022-09-11 16:02:37 -0500

    First Migration to AppDB

--- App/Controller/AppRepositories/AppContext.cs
+++ App/Controller/AppRepositories/AppContext.cs
@@ -7,10 +7,20 @@ using Model.entities;
 
 namespace Controller.AppRepositories
 {
-    public class AppContext : DbContext 
+    public class AppContext : DbContext //To use other methods and create the database. appContext inherits from the DbContext superclass
     {
         public DbSet<Municipio> Municipios {get;set;}
         public DbSet<Colegio> Colegios {get;set;}
         public DbSet<Patrocinador> Patrocinadores {get;set;}
+        //Method
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                //Connection string
+                //data Source parameter is the name of the servidor,initial Catalogo parameter is the name of the DB 
+                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = AppDB;");
+            }
+        }
     }
 }
---
+++ App/Controller/Migrations/20220911182018_First.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Controller.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Colegios",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    razonSocial = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    telefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ciudad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    direccion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colegios", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Municipios",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    secretaria = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Municipios", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patrocinadores",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    patrocinador = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    tipoPersona = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    telefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    correo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patrocinadores", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Colegios");
+
+            migrationBuilder.DropTable(
+                name: "Municipios");
+
+            migrationBuilder.DropTable(
+                name: "Patrocinadores");
+        }
+    }
+}
